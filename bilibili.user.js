a9000ae01561900b67babdd212aa5aeb7c412a8f
PermissionDog <permissiondog@qq.com>
2020-11-22 10:20:05 +0800

    .

---
+++ bilibili.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         WTBClient
+// @namespace    github.com/PermissionDog/WTBClient
+// @version      0.1
+// @description  一起看B客户端
+// @author       PermissionDog
+// @match        https://www.bilibili.com/*
+// @connect      bilibili.com
+// @connect      hdslb.com
+// @grant        GM_log
+// @grant        GM_xmlhttpRequest
+// @grant        GM_notification
+// @grant        window
+// @grant        document
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    GM_log("233");
+
+})();
